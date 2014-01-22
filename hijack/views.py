9f511b34e6db5f848c1505f9cf41e76fc4257a88
Philippe O. Wagner <wagner@arteria.ch>
2014-01-22 14:07:29 +0100

    Validation of userID - this fixes #5
    Thanks @mihaicc for reporting this issue. Will push a new release later today to PyPI.

--- hijack/views.py
+++ hijack/views.py
@@ -1,11 +1,15 @@
 from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
+from django.http import HttpResponseBadRequest
+
 
 from helpers import login_user
 
 @staff_member_required
 def login_with_id(request, userId):
+    if isinstance(userId, int):
+        return HttpResponseBadRequest('userId must be an integer value.')
     user = get_object_or_404(User, pk=userId)
     return login_user(request, user)
 
