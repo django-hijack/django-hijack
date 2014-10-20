f7893f96b79535047e1eb83910be259bf681c8fe
Simon Charette <charettes@users.noreply.github.com>
2014-10-20 15:11:32 -0400

    Avoid unnecessary DJANGO_VERSION checks

--- hijack/urls.py
+++ hijack/urls.py
@@ -1,11 +1,9 @@
-from django import VERSION as DJANGO_VERSION
-if float("%d.%d"%(DJANGO_VERSION[0], DJANGO_VERSION[1])) <= 1.5:
-    from django.conf.urls.defaults import patterns, url
-else:    
+try:
     from django.conf.urls import patterns, url
+except ImportError:
+    from django.conf.urls.defaults import patterns, url
 
  
-
 urlpatterns = patterns('hijack.views',
     url(r'^email/(?P<email>[\w.%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4})/$', 'login_with_email'),
     url(r'^username/(?P<username>\w+)/$', 'login_with_username'),
