274c02b24e81d8763148768cdf29184f40050828
Patrick Forringer <patrick@forringer.com>
2015-02-17 11:04:31 -0600

    adding names to urls, tests, update admin link to use named url

--- hijack/urls.py
+++ hijack/urls.py
@@ -1,9 +1,9 @@
 from compat import patterns, url
- 
+
 urlpatterns = patterns('hijack.views',
     url(r'^disable-hijack-warning/$', 'disable_hijack_warning', name='disable_hijack_warning'),
     url(r'^release-hijack/$', 'release_hijack', name='release_hijack'),
-    url(r'^email/(?P<email>[\w.%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4})/$', 'login_with_email'),
-    url(r'^username/(?P<username>\w+)/$', 'login_with_username'),
-    url(r'^(?P<userId>\w+)/$', 'login_with_id'),
+    url(r'^email/(?P<email>[\w.%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4})/$', 'login_with_email', name='login_with_email'),
+    url(r'^username/(?P<username>\w+)/$', 'login_with_username', name='login_with_username'),
+    url(r'^(?P<userId>\w+)/$', 'login_with_id', name='login_with_id'),
 )
