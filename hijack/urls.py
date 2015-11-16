2889bee9b83914f11486dc8c96d0d6cf21f39e16
Jannis Vamvas <vamvas@arteria.ch>
2015-11-16 11:35:02 +0100

    Refactor urls.py. Closes #91

--- hijack/urls.py
+++ hijack/urls.py
@@ -1,31 +1,19 @@
-from compat import patterns, url
+from compat import url
 
 from hijack import settings as hijack_settings
+from hijack.views import release_hijack, disable_hijack_warning, login_with_id, login_with_email, login_with_username
 
-
-urlpatterns = patterns('hijack.views',
-                       url(r'^release-hijack/$', 'release_hijack',
-                           name='release_hijack'), )
+urlpatterns = [
+    url(r'^release-hijack/$', release_hijack, name='release_hijack')
+]
 
 if hijack_settings.HIJACK_DISPLAY_WARNING:
-    urlpatterns += patterns('hijack.views',
-                            url(r'^disable-hijack-warning/$',
-                                view='disable_hijack_warning',
-                                name='disable_hijack_warning', ), )
+    urlpatterns.append(url(r'^disable-hijack-warning/$', disable_hijack_warning, name='disable_hijack_warning'))
 
 hijacking_user_attributes = hijack_settings.HIJACK_URL_ALLOWED_ATTRIBUTES
-
 if 'email' in hijacking_user_attributes:
-    urlpatterns += patterns('hijack.views',
-                            url(r'^email/(?P<email>[^@]+@[^@]+\.[^@]+)/$',
-                                view='login_with_email',
-                                name='login_with_email', ), )
+    urlpatterns.append(url(r'^email/(?P<email>[^@]+@[^@]+\.[^@]+)/$', login_with_email, name='login_with_email'))
 if 'username' in hijacking_user_attributes:
-    urlpatterns += patterns('hijack.views',
-                            url(r'^username/(?P<username>.*)/$',
-                                view='login_with_username',
-                                name='login_with_username', ), )
+    urlpatterns.append(url(r'^username/(?P<username>.*)/$', login_with_username, name='login_with_username'))
 if 'user_id' in hijacking_user_attributes:
-    urlpatterns += patterns('hijack.views', url(r'^(?P<user_id>[\w-]+)/$',
-                                                view='login_with_id',
-                                                name='login_with_id', ), )
+    urlpatterns.append(url(r'^(?P<user_id>[\w-]+)/$', login_with_id, name='login_with_id'))
