06a4770f93c622b73acb0035780c9bc5557395ff
Jannis Vamvas <vamvas@arteria.ch>
2016-04-11 13:22:12 +0200

    Re-format urls.py

--- hijack/urls.py
+++ hijack/urls.py
@@ -1,19 +1,39 @@
 from compat import url
 
 from hijack import settings as hijack_settings
-from hijack.views import release_hijack, disable_hijack_warning, login_with_id, login_with_email, login_with_username
+from hijack import views
 
 urlpatterns = [
-    url(r'^release-hijack/$', release_hijack, name='release_hijack')
+    url(
+        r'^release-hijack/$',
+        views.release_hijack,
+        name='release_hijack'
+    )
 ]
 
 if hijack_settings.HIJACK_DISPLAY_WARNING:
-    urlpatterns.append(url(r'^disable-hijack-warning/$', disable_hijack_warning, name='disable_hijack_warning'))
+    urlpatterns.append(url(
+        r'^disable-hijack-warning/$',
+        views.disable_hijack_warning,
+        name='disable_hijack_warning'
+    ))
 
 hijacking_user_attributes = hijack_settings.HIJACK_URL_ALLOWED_ATTRIBUTES
 if 'email' in hijacking_user_attributes:
-    urlpatterns.append(url(r'^email/(?P<email>[^@]+@[^@]+\.[^@]+)/$', login_with_email, name='login_with_email'))
+    urlpatterns.append(url(
+        r'^email/(?P<email>[^@]+@[^@]+\.[^@]+)/$',
+        views.login_with_email,
+        name='login_with_email'
+    ))
 if 'username' in hijacking_user_attributes:
-    urlpatterns.append(url(r'^username/(?P<username>.*)/$', login_with_username, name='login_with_username'))
+    urlpatterns.append(url(
+        r'^username/(?P<username>.*)/$',
+        views.login_with_username,
+        name='login_with_username'
+    ))
 if 'user_id' in hijacking_user_attributes:
-    urlpatterns.append(url(r'^(?P<user_id>[\w-]+)/$', login_with_id, name='login_with_id'))
+    urlpatterns.append(url(
+        r'^(?P<user_id>[\w-]+)/$',
+        views.login_with_id,
+        name='login_with_id'
+    ))
