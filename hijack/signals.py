8930090fc6198c327ec31c9da1b4f4afede0449f
Jannis Vamvas <vamvas@arteria.ch>
2017-05-15 16:31:58 +0200

    Remove deprecated signals from version 1. Resolves #115

--- hijack/signals.py
+++ hijack/signals.py
@@ -2,7 +2,3 @@ from django.dispatch import Signal
 
 hijack_started = Signal(providing_args=['hijacker_id', 'hijacked_id', 'request'])
 hijack_ended = Signal(providing_args=['hijacker_id', 'hijacked_id', 'request'])
-
-# Deprecated signals
-post_superuser_login = Signal(providing_args=['user_id'])
-post_superuser_logout = Signal(providing_args=['user_id'])
