dcfdb26a9efc0282b1d889a916d2fae3598d084e
Johannes Maron <info@johanneshoppe.com>
2021-01-30 08:54:29 +0100

    Remove deprecated providing_args
    The purely documentational providing_args argument for Signal is deprecated. If you rely on this argument as documentation, you can move the text to a code comment or docstring.

--- hijack/signals.py
+++ hijack/signals.py
@@ -1,5 +1,5 @@
 from django.dispatch import Signal
 
-hijack_started = Signal(providing_args=['hijacker_id', 'hijacked_id', 'request', 'hijacker', 'hijacked'])
-hijack_ended = Signal(providing_args=['hijacker_id', 'hijacked_id', 'request', 'hijacker', 'hijacked'])
+hijack_started = Signal()
+hijack_ended = Signal()
 
