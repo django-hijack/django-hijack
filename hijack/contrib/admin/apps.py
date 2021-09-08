cc3c682d38820d9a9df3dd7f0ce51f08b1537b79
Michael B Sopko <msopko@kmllawgroup.com>
2021-09-08 13:19:07 -0400

    Fix #328 -- Fix Proxy User Unregistering User Admin
    Co-Authored-By: Johannes Maron <johannes@maron.family>

--- hijack/contrib/admin/apps.py
+++ hijack/contrib/admin/apps.py
@@ -1,3 +1,5 @@
+import warnings
+
 from django.apps import AppConfig
 from django.contrib.auth import get_user_model
 
@@ -12,14 +14,24 @@ class HijackAdminConfig(AppConfig):
         from . import HijackUserAdminMixin
 
         UserModel = get_user_model()
-        UserModelAdmin = type(admin.site._registry[UserModel])
-        admin.site.unregister(UserModel)
+        try:
+            UserModelAdmin = type(admin.site._registry[UserModel])
+        except KeyError:
+            # User model may not be registered, see also:
+            # https://github.com/django-hijack/django-hijack/issues/328
+            warnings.warn(
+                f"{UserModel.__qualname__} is not registered in the default admin."
+                f" Please integrate hijack into your user admin manually.",
+                UserWarning,
+            )
+        else:
+            admin.site.unregister(UserModel)
 
-        # We create a subclass including the HijackUserAdminMixin but keep the name
-        # and module, to keep output form failing checks consistent.
-        HijackUserModelAdmin = type(
-            UserModelAdmin.__name__, (HijackUserAdminMixin, UserModelAdmin), {}
-        )
-        HijackUserModelAdmin.__module__ = UserModelAdmin.__module__
+            # We create a subclass including the HijackUserAdminMixin but keep the name
+            # and module, to keep output form failing checks consistent.
+            HijackUserModelAdmin = type(
+                UserModelAdmin.__name__, (HijackUserAdminMixin, UserModelAdmin), {}
+            )
+            HijackUserModelAdmin.__module__ = UserModelAdmin.__module__
 
-        admin.site.register(UserModel, HijackUserModelAdmin)
+            admin.site.register(UserModel, HijackUserModelAdmin)
--- hijack/tests/test_admin.py
+++ hijack/tests/test_admin.py
@@ -1,9 +1,11 @@
 from unittest.mock import MagicMock
 
+import pytest
 from django.urls import reverse
 
 from hijack.contrib.admin import HijackUserAdminMixin
-from hijack.tests.test_app.models import Post
+from hijack.contrib.admin.apps import HijackAdminConfig
+from hijack.tests.test_app.models import CustomUser, Post
 
 
 class TestHijackUserAdminMixin:
@@ -12,6 +14,18 @@ class TestHijackUserAdminMixin:
         response = admin_client.get(url)
         assert response.status_code == 200
         assert b"data-hijack-user" in response.content
+
+    @pytest.fixture()
+    def no_user_admin(self, settings):
+        from django.contrib import admin
+
+        CustomUserAdmin = admin.site._registry.pop(CustomUser)
+        yield
+        admin.site._registry[CustomUser] = CustomUserAdmin
+
+    def test_user_admin__unregistered(self, no_user_admin, admin_client):
+        with pytest.warns(UserWarning, match="CustomUser is not registered"):
+            HijackAdminConfig.ready(None)
 
     def test_related_user(self, admin_client, admin_user):
         url = reverse("admin:test_app_post_changelist")
