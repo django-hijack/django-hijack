3915a5cac889a550017da970934a36d46d29d9a9
Johannes Maron <johannes@maron.family>
2022-12-27 14:30:17 +0000

    Fix #520 -- Add support for custom managers on user model

--- hijack/tests/test_app/models.py
+++ hijack/tests/test_app/models.py
@@ -1,12 +1,19 @@
 import uuid
 
 from django.conf import settings
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, UserManager
 from django.db import models
+
+
+class CustomUserManager(UserManager):
+    def select_for_update(self):
+        raise NotImplementedError
 
 
 class CustomUser(AbstractUser):
     uuid = models.UUIDField(editable=False, default=uuid.uuid4, primary_key=True)
+
+    objects = CustomUserManager()
 
 
 class Post(models.Model):
