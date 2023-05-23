c02edeffc3d95e9f5c4f9ab3017b93347e3e5240
Johannes Maron <johannes@maron.family>
2023-05-23 15:33:12 +0200

    Update support to current Django & Python versions

--- hijack/tests/test_app/settings.py
+++ hijack/tests/test_app/settings.py
@@ -28,15 +28,6 @@ NOSE_ARGS = []
 
 TEMPLATE_DIRS = (os.path.join(APP_ROOT, "tests/test_app/templates"),)
 
-PASSWORD_HASHERS = (
-    "django.contrib.auth.hashers.PBKDF2PasswordHasher",
-    "django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher",
-    "django.contrib.auth.hashers.BCryptPasswordHasher",
-    "django.contrib.auth.hashers.SHA1PasswordHasher",
-    "django.contrib.auth.hashers.MD5PasswordHasher",
-    "django.contrib.auth.hashers.CryptPasswordHasher",
-)
-
 MIDDLEWARE = (
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
