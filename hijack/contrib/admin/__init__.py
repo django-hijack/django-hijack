a024728c4f334efc72ddf97630a2493802afd6f5
Pascal F <pfouque@users.noreply.github.com>
2023-12-04 21:13:30 +0100

    Cleanup code for unsupported Django versions

--- hijack/contrib/admin/__init__.py
+++ hijack/contrib/admin/__init__.py
@@ -1,6 +1,3 @@
-import django
-
-if django.VERSION < (3, 2):
-    default_app_config = "hijack.contrib.admin.apps.HijackAdminConfig"
-
 from .admin import HijackUserAdminMixin  # noqa
+
+__all__ = ["HijackUserAdminMixin"]
