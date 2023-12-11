django-hijack/django-hijack

commit a36fbb8fa71206ad01d330e9f34bb321d5340f82

Fix #613 -- Lock single row instead of full user table
Some databases showed poor performance locking the whole user table, especially on considerable table sizes.
To mitigate this issue, we changed to locking only a single users' row. The current hijacker will be locked, until the transaction is completed. The transaction wraps the entire request. Thanks, @vinodpandey, for reporting the issue and providing a patch.

hijack/views.py
@@ -63,8 +63,8 @@ class SuccessUrlMixin:
 class LockUserTableMixin:
     @transaction.atomic()
     def dispatch(self, request, *args, **kwargs):
-        # Lock entire user table to avoid race conditions
-        next(get_user_model()._base_manager.select_for_update().iterator())
+        # Lock user row to avoid race conditions
+        get_user_model()._base_manager.select_for_update().get(pk=request.user.pk)
         return super().dispatch(request, *args, **kwargs)
 
 

hijack/tests/test_views.py
@@ -18,7 +18,9 @@ class TestLockUserTableMixin:
         class LockedView(views.LockUserTableMixin, View):
             pass
 
-        LockedView().dispatch(rf.get("/"))
+        request = rf.get("/")
+        request.user = admin_user
+        LockedView().dispatch(request)
 
 
 class TestSuccessUrlMixin:

commit 8e32946d0a14196eb94984a56ed138218b726226

Ref #613 -- Handle unauthenticated users in hijack views (#629)
If the user is not authenticated, `LockUserTableMixin.dispatch` raises a `User.DoesNotExist` error.

Regression from a36fbb8

hijack/views.py
@@ -69,8 +69,8 @@ class LockUserTableMixin:
 
 
 class AcquireUserView(
-    LockUserTableMixin,
     LoginRequiredMixin,
+    LockUserTableMixin,
     UserPassesTestMixin,
     SuccessUrlMixin,
     SingleObjectMixin,
@@ -117,7 +117,11 @@ class AcquireUserView(
 
 
 class ReleaseUserView(
-    LockUserTableMixin, LoginRequiredMixin, UserPassesTestMixin, SuccessUrlMixin, View
+    LoginRequiredMixin,
+    LockUserTableMixin,
+    UserPassesTestMixin,
+    SuccessUrlMixin,
+    View,
 ):
     raise_exception = True
 

hijack/tests/test_views.py
@@ -2,9 +2,12 @@ import datetime
 import time
 from importlib import import_module
 from unittest.mock import MagicMock
+from urllib.parse import urlencode, urlunparse
 
 import pytest
+from django.conf import settings
 from django.contrib.sessions.models import Session
+from django.shortcuts import resolve_url
 from django.urls import reverse_lazy
 from django.utils import timezone
 from django.views import View
@@ -81,6 +84,20 @@ class TestAcquireUserView:
         response = admin_client.post(self.url)
         assert response.status_code == 400
 
+    def test_unauthenticated(self, client, bob):
+        response = client.post(self.url, {"user_pk": bob.pk})
+        assert response.status_code == 302
+        assert response["Location"] == urlunparse(
+            (
+                "",
+                "",
+                resolve_url(settings.LOGIN_URL),
+                "",
+                urlencode({"next": self.url}, safe="/"),
+                "",
+            ),
+        )
+
 
 class TestReleaseUserView:
     release_url = reverse_lazy("hijack:release")
@@ -130,6 +147,10 @@ class TestReleaseUserView:
         response = bob_client.post(self.release_url, {"next": "/somewhere/else"})
         assert response.status_code == 302
         assert response["Location"] == "/somewhere/else"
+
+    def test_unauthenticated(self, client, alice):
+        response = client.post(self.release_url, {"user_pk": alice.pk})
+        assert response.status_code == 403
 
 
 class TestIntegration: